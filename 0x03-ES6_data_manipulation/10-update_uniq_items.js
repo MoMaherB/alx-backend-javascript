8808696393c0783ef996d7b01b8da0c4a7c4ce66
MoMaherB <maher1525@gmail.com>
2024-10-08 21:40:06 +0300

    zsz

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 const updateUniqueItems = (items) => {
   if (!(items instanceof Map)) {
-	  throw new Error('Cannot process');
+    throw new Error('Cannot process');
   }
   for (const [k, v] of items.entries()) {
-	  if (v === 1) {
+    if (v === 1) {
       items.set(k, 100);
-	  }
+    }
   }
   return items;
 };
