696a45771f5f9d6819975e5a766b78188a31d760
MoMaherB <maher1525@gmail.com>
2024-10-09 21:21:10 +0300

    final

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,6 @@
 const updateUniqueItems = (items) => {
   if (!(items instanceof Map)) {
-    throw new Error('Cannot proces');
+    throw new Error('Cannot process');
   }
   for (const [k, v] of items.entries()) {
     if (v === 1) {
